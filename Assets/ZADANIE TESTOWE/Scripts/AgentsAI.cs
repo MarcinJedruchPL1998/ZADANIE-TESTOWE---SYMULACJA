5bbfca64e817bb217d45592ea0af2ca05a744e9f
MarcinJedruchPL1998 <m.jedruch481998@gmail.com>
2022-04-16 09:11:12 +0200

    commit_03

--- Assets/ZADANIE TESTOWE/Scripts/AgentsAI.cs
+++ Assets/ZADANIE TESTOWE/Scripts/AgentsAI.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class AgentsAI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject spawnArea;
+    [SerializeField] private float moveSpeed;
+    Vector3 randomPos;
+
     void Start()
     {
-        
+        SetRandomPosition();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position != randomPos)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, randomPos, moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            SetRandomPosition();
+        }
+    }
+
+    public Vector3 SetRandomPosition()
+    {
+        float x = spawnArea.transform.localScale.x;
+        float z = spawnArea.transform.localScale.z;
+        float randomPosX = Random.Range(-x / 2, x / 2);
+        float randomPosZ = Random.Range(-z / 2, z / 2);
+        randomPos = new Vector3(randomPosX, spawnArea.transform.position.y, randomPosZ);
+        transform.LookAt(randomPos);
+
+        return randomPos;
     }
 }
